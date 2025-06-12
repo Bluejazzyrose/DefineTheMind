8311f05251b973754c0972c64899e0a2a2615aac
Jasmine Frye <156701047+Bluejazzyrose@users.noreply.github.com>
2025-06-12 13:07:04 -0500

    Update and rename quizMBTI.js to quiz.js

--- quiz.js
+++ quiz.js
@@ -11,38 +11,35 @@
 
 window.addEventListener("load", buildQuiz);
 
-// variables to keep track of quiz stat answers
-let se = 0;
-let si = 0;
-let ne = 0;
-let ni = 0;
-let fe = 0;
-let fi = 0;
-let te = 0;
-let ti = 0;
-let pe = 0;
-let pi = 0;
-let je = 0;
-let ji = 0;
-
 // counter to cycle through quiz questions
 let counter = 0;
 
 // parts of the quiz field
-	let question = document.getElementById("question");
-	let ans1 = document.getElementById("ans1");
-	let ans2 = document.getElementById("ans2");
-	let ans3 = document.getElementById("ans3");
-	let ans4 = document.getElementById("ans4");
+let question = document.getElementById("question");
+let ans1 = document.getElementById("ans1");
+let ans2 = document.getElementById("ans2");
+let ans3 = document.getElementById("ans3");
+let ans4 = document.getElementById("ans4");
 	
 
 // function to set up 1 question
 function askQuestion() {
-	if (counter > 29) {
+	if (counter < qTexts.length) {
+		question.innerHTML = qTexts[counter];
+		ans1.innerHTML = aAnswers[counter];
+		ans2.innerHTML = bAnswers[counter];
+		if (cAnswers[counter] === undefined) {
+			ans3.id = "null3";
+			ans4.id = "null4";
+		} else {
+			ans3.innerHTML = cAnswers[counter];
+			ans4.innerHTML = dAnswers[counter];
+			ans3.id = "ans3";
+			ans4.id = "ans4";
+		}
+	} else {
 		calculate();
 		
-		// display Type result
-		question.innerHTML = "We have defined your mind as: " + acronym[0] + acronym[1] + acronym[2] + acronym[3];
 		ans1.innerHTML = "Home"
 		ans2.innerHTML = "Retake the Quiz"
 		
@@ -53,26 +50,13 @@ function askQuestion() {
 		ans2.onclick = function() {
 			window.location.href = "quizMBTI.html";
 		}
-	} else {
-		question.innerHTML = qTexts[counter];
-		ans1.innerHTML = aAnswers[counter];
-		ans2.innerHTML = bAnswers[counter];
-		if (counter === 16 || counter === 17) {
-			ans3.innerHTML = cAnswers[counter];
-			ans4.innerHTML = dAnswers[counter];
-			ans3.id = "ans3";
-			ans4.id = "ans4";
-		} else {
-			ans3.id = "null3";
-			ans4.id = "null4";
-		}
 	}
 }
 
 
 // function to build and run the quiz
 function buildQuiz() {
-	//set up the first question
+	// set up the first question
 	askQuestion();
 	
 	// if ans1 is clicked
